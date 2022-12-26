325a0b544d927ed8e378ca68feb8697f621ac21c
mitesh2205 <48359593+mitesh2205@users.noreply.github.com>
2022-12-25 23:08:51 -0800

    HappyNumber created

---
+++ happy_number.py
@@ -0,0 +1,9 @@
+def happynumber(nums):
+    seen = set()
+    while nums!=1 or nums not in seen:
+        seen.add(nums)
+        nums = sum(int(i)** 2 for i in str(nums))
+
+    return nums == 1
+
+    

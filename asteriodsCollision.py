1deab6826cbc1ba12c59a91ff24d107da39f53fb
mitesh2205 <48359593+mitesh2205@users.noreply.github.com>
2023-01-11 23:39:52 -0800

    Update in asteriod collision and written basic calculator 2 in problems.ipynb

--- asteriodsCollision.py
+++ asteriodsCollision.py
@@ -1,10 +1,25 @@
 def asteriodCollision(asteriods):
     stack =[]
-
     for a in asteriods:
-        while stack and stack[-1]>0 and a<0:
+        while stack and stack[-1]>0 and a<0: # if stack is not empty and last element is positive and current element is negative
             if stack[-1] + a < 0: stack.pop()
             elif stack[-1] + a > 0: break
             else: stack.pop(); break
         else: stack.append(a)
     return stack
+
+# brute force method of solving asteriod collision
+
+def asteriodCollision(asteriods):
+    stack =[]
+    for a in asteriods:
+        if a>0: stack.append(a)
+        else:
+            while stack and stack[-1]>0 and a<0:
+                if stack[-1] + a < 0: stack.pop()
+                elif stack[-1] + a > 0: break
+                else: stack.pop(); break
+            else: stack.append(a)
+    return stack
+
+# TC: O(n^2) SC: O(n) where n is the length of asteriods

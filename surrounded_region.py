e0a36fe6bf898698acd41a7d9729cf77fdd547c5
mitesh2205 <48359593+mitesh2205@users.noreply.github.com>
2023-01-18 15:11:30 -0800

    Updated surrounded_region

--- surrounded_region.py
+++ surrounded_region.py
@@ -2,27 +2,30 @@ def surrounded_region(borad):
     row, col = len(board), len(board[0])
 
     def dfs(i, j):
-        if i in ([0,row-1] or j in [0,col-1]) and board[i][j] == 'O':
-            board[i][j] = 'D'
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
+        if i < 0 or j < 0 or i == row or j == col or board[i][j] != "O":
+            return
+        board[i][j] = 'D'
+        dfs(i+1, j)
+        dfs(i-1, j)
+        dfs(i, j+1)
+        dfs(i, j-1)
 
     for i in range(row):
         for j in range(col):
-            if board[i][j] == "O":
+            if board[i][j] == "O" and (i == 0 or i == row - 1 or j == 0 or j == col - 1):
                 dfs(i, j)
 
     for i in range(row):
         for j in range(col):
-            if borad[i][j] == "O":
+            if board[i][j] == "O":
                 board[i][j] = "X"
 
     for i in range(row):
         for j in range(col):
             if board[i][j] == "D":
                 board[i][j] = "O"
+
+    return board
 
 
 print(surrounded_region([["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]))

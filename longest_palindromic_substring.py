0e99c371ca09e935ebf135bc45ff91c5b85e1cb9
mitesh2205 <48359593+mitesh2205@users.noreply.github.com>
2023-01-28 18:09:52 -0800

    Create longest_palindromic_substring.py

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,34 @@
+def longest_palindromic_substring(s):
+    if len(s) == 0:
+        return ""
+
+    res = ""
+    res_length = 0
+
+    for i in range(len(s)):
+        #even length
+
+        left = i
+        right = i
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if right - left + 1 > res_length:
+                res = s[left:right+1]
+                res_length = right - left + 1
+            left -= 1
+            right += 1
+
+        # odd length
+
+        left, right = i, i+1
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if right - left + 1 > res_length:
+                res = s[left:right+1]
+                res_length = right - left + 1
+            left -= 1
+            right += 1
+
+    return res
+
+print(longest_palindromic_substring("babadar"))

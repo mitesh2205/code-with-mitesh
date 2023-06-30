09339ee770ae03f69d62e8b7877d2535622e2a7d
Mitesh Maheshbhai Chhatbar <mchhatba@usc.edu>
2023-06-29 21:46:11 -0700

    Trapping Rain water Created
    Approach two pointer

---
+++ JavaCode/Arrays/container_with_most_water.java
@@ -0,0 +1,24 @@
+package JavaCode.Arrays;
+
+public class container_with_most_water {
+    public static void most_water(int height[]) {
+        int area = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            area = Math.max(area, Math.min(height[left], height[right]) * Math.abs(right - left));
+            if (height[left] < height[right]) {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        most_water(height);
+    }
+}
